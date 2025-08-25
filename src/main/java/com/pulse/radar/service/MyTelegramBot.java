5d42fb35a429228818826e54d2ccaf0bab6c3430
sait.demirkaya <sait.demirkaya@hotmail.com>
2025-08-25 23:56:23 +0300

    Radar in!

--- src/main/java/com/pulse/radar/service/MyTelegramBot.java
+++ src/main/java/com/pulse/radar/service/MyTelegramBot.java
@@ -8,11 +8,24 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
 public class MyTelegramBot extends TelegramLongPollingBot {
 
+    public static final String BALL_POSSESSION = "ball-possession";
+    public static final String SHOTS_TOTAL = "shots-total";
+    public static final String SHOTS_ON_TARGET = "shots-on-target";
+    public static final String SHOTS_OFF_TARGET = "shots-off-target";
+    public static final String SHOTS_BLOCKED = "shots-blocked";
+
+    List<String> teamName = new ArrayList<>();
+    List<String> yuksekVerimTeams = new ArrayList<>();
+
+
+    double verim = 5.6;
+    double buyukVerim = 9.5;
     private final SportMonksService sportMonksService;
 
     public MyTelegramBot(SportMonksService sportMonksService) {
@@ -44,47 +57,38 @@ public class MyTelegramBot extends TelegramLongPollingBot {
     @Override
     public void onUpdatesReceived(List<Update> updates) {
 
+        while (true) {
+            SendMessage sendMessage;
+            StringBuilder result = new StringBuilder();
             try {
-                SendMessage sendMessage = new SendMessage();
+                sendMessage = new SendMessage();
                 sendMessage.setChatId("-636719291");
                 LiveScoresResponse response = sportMonksService.getLiveMatches();
 
                 for (LiveScoresResponse.MatchData datum : response.getData()) {
                     FixtureResponse matchStatistic = sportMonksService.getMatchStatistic(datum.getId());
+                    result.append(parseAndPrint(matchStatistic, datum.getId()));
                 }
-                StringBuilder sb = new StringBuilder("📊 Canlı Maçlar:\n\n");
-                response.getData().forEach(match ->
-                        sb.append("⚽ ").append(match.getName())
-                                .append(" ⏰ ").append(match.getStartingAt())
-                                .append("\n")
-                );
+            /*StringBuilder sb = new StringBuilder("📊 Canlı Maçlar:\n\n");
+            response.getData().forEach(match ->
+                    sb.append("⚽ ").append(match.getName())
+                            .append(" ⏰ ").append(match.getStartingAt())
+                            .append("\n")
+            );*/
+            /*if (result.isEmpty()) {
+                sendMessage.setText("Veri bulunamadı!");
+            } else {
+                sendMessage.setText(result);
+            }*/
 
+                if (result.length() > 0) {
+                    sendMessage.setText(result.toString());
+                    System.out.println("Sonuç mesajı: " + result);
+                } else {
+                    sendMessage.setText("Selam Görkem. Beni özledin mi?");
+                }
                 execute(sendMessage);
             } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-
-        /*ProcessMatch processMatch = new ProcessMatch();
-        while (true) {
-            SendMessage sendMessage;
-
-            try {
-                System.out.println("\n ARAMA BAŞLADI \n");
-
-                LiveScoresResponse liveScores = calculateStatistics.getLiveScores();
-                Map<TeamsValues, List<StatusValue>> searchResultMap = new SendReqToWebSite().getMatchStatistics();
-                sendMessage = new SendMessage();
-                sendMessage.setChatId("-636719291");
-                String result = processMatch.calculate(searchResultMap, teamGoals);
-                if (!result.isEmpty()) {
-                    sendMessage.setText(result);
-                    try {
-                        execute(sendMessage);
-                    } catch (TelegramApiException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } catch (Exception e) {
                 System.out.println(e);
                 try {
                     sendMessage = new SendMessage();
@@ -95,34 +99,271 @@ public class MyTelegramBot extends TelegramLongPollingBot {
                 } catch (InterruptedException interruptedException) {
                     interruptedException.printStackTrace();
                 } catch (TelegramApiException ex) {
-                    System.out.println("Hata mesajı atarken de hata alındı!");
-                    ;
+                    System.out.println("Hata mesajı atarken de hata alındı!");;
                 }
-                continue;
             }
-
-
             try {
-                long LOWER_RANGE = 30000; //assign lower range value
-                long UPPER_RANGE = 90000; //assign upper range value
-                Random random = new Random();
-
-
-                long randomValue = LOWER_RANGE +
-                        (long) (random.nextDouble() * (UPPER_RANGE - LOWER_RANGE));
+                long ONE_MINUTE = 60000; // 1 minute in milliseconds
                 Calendar calendar = Calendar.getInstance(); // gets a calendar using the default time zone and locale.
                 System.out.println("Şu anki Tarama dk'sı: " + calendar.getTime());
-                calendar.add(Calendar.SECOND, (int) randomValue / 1000);
+                calendar.add(Calendar.SECOND, 60); // Add 1 minute
                 System.out.println("Sıradaki Tarama dk'sı: " + calendar.getTime());
                 System.out.println("----------------------------------------------------------------");
-                Thread.sleep(randomValue);
+                Thread.sleep(ONE_MINUTE);
             } catch (InterruptedException e) {
                 System.exit(0);
             }
+        }
+    }
 
-        }*/
+    private String parseAndPrint(FixtureResponse fixtureResponse, long fixtureId) {
+
+        List<String> targetCodes = Arrays.asList(BALL_POSSESSION, SHOTS_TOTAL, SHOTS_ON_TARGET, SHOTS_OFF_TARGET, SHOTS_BLOCKED);
+
+        String[] teams = fixtureResponse.getData().getName().split(" vs ");
+        String homeTeamName = teams[0].trim();
+        String awayTeamName = teams[1].trim();
+        // Get the statistics from the response
+        System.out.println("\n \nMatch: " + fixtureResponse.getData().getName());
+        List<FixtureResponse.DataObject.Statistic> statistics = fixtureResponse.getData().getStatistics();
+
+        if (statistics == null || statistics.isEmpty()) {
+            System.out.println("No statistics available for this match: " + fixtureResponse.getData().getName());
+            return "";
+        }
+
+        // Extract home statistics
+        System.out.println("\nHome Statistics:");
+        Map<String, Integer> homeValues = extractValuesByLocation(statistics, "home", targetCodes);
+        homeValues.forEach((code, value) -> System.out.println("Code: " + code + ", Value: " + value));
+
+        // Extract away statistics
+        System.out.println("\nAway Statistics:");
+        Map<String, Integer> awayValues = extractValuesByLocation(statistics, "away", targetCodes);
+        awayValues.forEach((code, value) -> System.out.println("Code: " + code + ", Value: " + value));
+        int matchMinute = sportMonksService.getMatchMinute(fixtureId);
+        System.out.println("\nMatch Minute: " + matchMinute);
+        String matchScore = sportMonksService.getMatchScore(fixtureId);
+        System.out.println("\nMatch Score: " + matchScore);
+        return calculate(homeValues, awayValues, matchMinute, matchScore, homeTeamName, awayTeamName);
 
     }
+
+    private String calculate(Map<String, Integer> homeValues, Map<String, Integer> awayValues, int min, String matchScore, String homeTeamName, String awayTeamName) {
+
+        StringBuilder result = new StringBuilder("");
+
+        String[] split = matchScore.split("-");
+        String homeScore = split[0].trim();
+        String awayScore = split[1].trim();
+        if (!homeScore.equals("0") && !awayScore.equals("0")) {
+            return "";
+        }
+
+        if (min > 75 || min < 15) {
+            return "";
+        }
+
+
+        Integer homeTotalShot = homeValues.get(SHOTS_TOTAL);
+        int homeToplaOynamaVeSut = homeValues.get(BALL_POSSESSION) * homeTotalShot;
+        double homeIsabetOrani = getIsabetOrani(homeValues.get(SHOTS_ON_TARGET), homeTotalShot);
+        double homeVerim = (double) homeToplaOynamaVeSut / min;
+
+        Integer awayTotalShot = awayValues.get(SHOTS_TOTAL);
+        int awayToplaOynamaVeSut = awayValues.get(BALL_POSSESSION) * awayTotalShot;
+        double awayIsabetOrani = getIsabetOrani(awayValues.get(SHOTS_ON_TARGET), awayTotalShot);
+        double awayVerim = (double) awayToplaOynamaVeSut / min;
+
+
+        if (min >= 15 && min <= 20) {
+            if ((homeTotalShot >= 5 &&
+                    homeIsabetOrani <= 2 &&
+                    homeVerim >= 16) &&
+                    !teamName.contains(homeTeamName) &&
+                    homeScore.equals("0")) {
+
+                teamName.add(homeTeamName);
+                result.append("Dakika : ").append(min).append(" ");
+                result.append(homeTeamName).append("-").append(awayTeamName).append(" maçında ");
+                result.append(homeTeamName).append(" takımının bir golü (0.5 üst) yüksek güvenden alınabilir bol şans!\n\n");
+                System.out.println(result);
+            }
+            if (((awayTotalShot) >= 5 &&
+                    awayIsabetOrani <= 2 &&
+                    awayVerim >= 16) &&
+                    !teamName.contains(awayTeamName) &&
+                    awayScore.equals("0")) {
+                teamName.add(awayTeamName);
+                result.append("Dakika : ").append(min).append(" ");
+                result.append(homeTeamName).append("-").append(awayTeamName).append(" maçında ");
+                result.append(awayTeamName).append(" takımının bir golü (0.5 üst) yüksek güvenden alınabilir bol şans!\n\n");
+                System.out.println(result);
+            }
+        } else if (min >= 21 && min <= 25) {
+            if (((homeTotalShot) >= 5 &&
+                    homeIsabetOrani <= 2 &&
+                    homeVerim >= 14) &&
+                    !teamName.contains(homeTeamName) &&
+                    homeScore.equals("0")) {
+
+                teamName.add(homeTeamName);
+                result.append("Dakika : ").append(min).append(" ");
+                result.append(homeTeamName).append("-").append(awayTeamName).append(" maçında ");
+                result.append(homeTeamName).append(" takımının bir golü (0.5 üst) yüksek güvenden alınabilir bol şans!\n\n");
+                System.out.println(result);
+            }
+            if (((awayTotalShot) >= 5 &&
+                    awayIsabetOrani <= 2 &&
+                    awayVerim >= 14) &&
+                    !teamName.contains(awayTeamName) &&
+                    awayScore.equals("0")) {
+                teamName.add(awayTeamName);
+                result.append("Dakika : ").append(min).append(" ");
+                result.append(homeTeamName).append("-").append(awayTeamName).append(" maçında ");
+                result.append(awayTeamName).append(" takımının bir golü (0.5 üst) yüksek güvenden alınabilir bol şans!\n\n");
+                System.out.println(result);
+            }
+        } else if (min >= 26 && min <= 30) {
+            if (((homeTotalShot) >= 5 &&
+                    homeIsabetOrani <= 2 &&
+                    homeVerim >= 12) &&
+                    !teamName.contains(homeTeamName) &&
+                    homeScore.equals("0")) {
+
+                teamName.add(homeTeamName);
+                result.append("Dakika : ").append(min).append(" ");
+                result.append(homeTeamName).append("-").append(awayTeamName).append(" maçında ");
+                result.append(homeTeamName).append(" takımının bir golü (0.5 üst) yüksek güvenden alınabilir bol şans!\n\n");
+                System.out.println(result);
+            }
+            if (((awayTotalShot) >= 5 &&
+                    awayIsabetOrani <= 2 &&
+                    awayVerim >= 12) &&
+                    !teamName.contains(awayTeamName) &&
+                    awayScore.equals("0")) {
+                teamName.add(awayTeamName);
+                result.append("Dakika : ").append(min).append(" ");
+                result.append(homeTeamName).append("-").append(awayTeamName).append(" maçında ");
+                result.append(awayTeamName).append(" takımının bir golü (0.5 üst) yüksek güvenden alınabilir bol şans!\n\n");
+                System.out.println(result);
+            }
+        } else if (min >= 31 && min <= 45) {
+            if (((homeTotalShot) >= 6 &&
+                    homeIsabetOrani <= 2 &&
+                    homeVerim >= 8) &&
+                    !teamName.contains(homeTeamName) &&
+                    homeScore.equals("0")) {
+
+                teamName.add(homeTeamName);
+                result.append("Dakika : ").append(min).append(" ");
+                result.append(homeTeamName).append("-").append(awayTeamName).append(" maçında ");
+                result.append(homeTeamName).append(" takımının bir golü (0.5 üst) yüksek güvenden alınabilir bol şans!\n\n");
+                System.out.println(result);
+            }
+            if (((awayTotalShot) >= 6 &&
+                    awayIsabetOrani <= 2 &&
+                    awayVerim >= 8) &&
+                    !teamName.contains(awayTeamName) &&
+                    awayScore.equals("0")) {
+                teamName.add(awayTeamName);
+                result.append("Dakika : ").append(min).append(" ");
+                result.append(homeTeamName).append("-").append(awayTeamName).append(" maçında ");
+                result.append(awayTeamName).append(" takımının bir golü (0.5 üst) yüksek güvenden alınabilir bol şans!\n\n");
+                System.out.println(result);
+            }
+        }
+        if (min >= 46 && min <= 60) {
+            if (((homeTotalShot) >= 7 &&
+                    homeIsabetOrani <= 3 &&
+                    homeVerim >= 6) &&
+                    !teamName.contains(homeTeamName) &&
+                    homeScore.equals("0")) {
+
+                teamName.add(homeTeamName);
+                result.append("Dakika : ").append(min).append(" ");
+                result.append(homeTeamName).append("-").append(awayTeamName).append(" maçında ");
+                result.append(homeTeamName).append(" takımının bir golü (0.5 üst) yüksek güvenden alınabilir bol şans!\n\n");
+                System.out.println(result);
+            }
+            if (((awayTotalShot) >= 7 &&
+                    awayIsabetOrani <= 3 &&
+                    awayVerim >= 6) &&
+                    !teamName.contains(awayTeamName) &&
+                    awayScore.equals("0")) {
+                teamName.add(awayTeamName);
+                result.append("Dakika : ").append(min).append(" ");
+                result.append(homeTeamName).append("-").append(awayTeamName).append(" maçında ");
+                result.append(awayTeamName).append(" takımının bir golü (0.5 üst) yüksek güvenden alınabilir bol şans!\n\n");
+                System.out.println(result);
+            }
+        }
+        if (min >= 61 && min <= 75) {
+            if (((homeTotalShot) >= 8 &&
+                    homeIsabetOrani <= 3 &&
+                    homeVerim >= 5) &&
+                    !teamName.contains(homeTeamName) &&
+                    homeScore.equals("0")) {
+
+                teamName.add(homeTeamName);
+                result.append("Dakika : ").append(min).append(" ");
+                result.append(homeTeamName).append("-").append(awayTeamName).append(" maçında ");
+                result.append(homeTeamName).append(" takımının bir golü (0.5 üst) yüksek güvenden alınabilir bol şans!\n\n");
+                System.out.println(result);
+            }
+            if (((awayTotalShot) >= 8 &&
+                    awayIsabetOrani <= 3 &&
+                    awayVerim >= 5) &&
+                    !teamName.contains(awayTeamName) &&
+                    awayScore.equals("0")) {
+                teamName.add(awayTeamName);
+                result.append("Dakika : ").append(min).append(" ");
+                result.append(homeTeamName).append("-").append(awayTeamName).append(" maçında ");
+                result.append(awayTeamName).append(" takımının bir golü (0.5 üst) yüksek güvenden alınabilir bol şans!\n\n");
+                System.out.println(result);
+            }
+        }
+
+
+        if (min >= 15 && min <= 25) {
+
+            boolean homeVerimBuyukSonuc = (double) homeToplaOynamaVeSut / min >= 20;
+            if (homeVerimBuyukSonuc && homeIsabetOrani <= 2 && (homeTotalShot) > 4) {
+
+                if (!yuksekVerimTeams.contains(homeTeamName) && homeScore.equals("0")) {
+                    yuksekVerimTeams.add(homeTeamName);
+                    result.append(homeTeamName).append("-").append(awayTeamName).append(" maçında ");
+                    result.append("ilk yarı bir gol (0.5 üst) orta güvenden alınabilir bol şans!\n\n");
+                    System.out.println(result);
+                }
+            }
+
+            boolean awayVerimBuyukSonuc = awayVerim >= 20;
+            if (awayVerimBuyukSonuc && awayIsabetOrani <= 2 && (awayTotalShot) > 4) {
+                if (!yuksekVerimTeams.contains(awayTeamName) && awayScore.equals("0")) {
+                    yuksekVerimTeams.add(awayTeamName);
+                    result.append(homeTeamName).append("-").append(awayTeamName).append(" maçında ");
+                    result.append("ilk yarı bir gol (0.5 üst) orta güvenden alınabilir bol şans!\n\n");
+                    System.out.println(result);
+                }
+            }
+
+        }
+        return result.toString();
+
+    }
+
+    private static Map<String, Integer> extractValuesByLocation(List<FixtureResponse.DataObject.Statistic> statistics, String location, List<String> targetCodes) {
+        return statistics.stream()
+                .filter(stat -> location.equals(stat.getLocation()) && targetCodes.contains(stat.getType().getCode()))
+                .collect(Collectors.toMap(stat -> stat.getType().getCode(), stat -> stat.getData().getValue()));
+    }
+
+    private double getIsabetOrani(int homeTeamİsabetli, int homeTotalShot) {
+        return homeTeamİsabetli == 0 ?
+                9.9 : (double) (homeTotalShot) / homeTeamİsabetli;
+    }
+
 
     @Override
     public String getBotUsername() {
--- src/main/java/com/pulse/radar/service/SportMonksService.java
+++ src/main/java/com/pulse/radar/service/SportMonksService.java
@@ -1,5 +1,6 @@
 package com.pulse.radar.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pulse.radar.model.FixtureResponse;
 import com.pulse.radar.model.LiveScoresResponse;
@@ -16,6 +17,11 @@ public class SportMonksService {
             "api_token=6bUSKxpE0SRqcvGdK8cIvzyO7G7VB5ibpnjzSzKM7XSRS5hluHGNv6x2XFWc";
     private static final String STATISTIC =
             "?include=statistics.type";
+
+    private static final String MINUTE =
+            "?include=periods;state";
+    private static final String SCORE =
+            "?include=scores";
     private static final String LIVE_MATCH_URL =
             "https://api.sportmonks.com/v3/football/livescores/inplay" + "?" + API_TOKEN;
     private static final String MATCH_STATISTICS_URL =
@@ -37,10 +43,77 @@ public class SportMonksService {
             throw new RuntimeException("SportMonks API çağrısı başarısız oldu", e);
         }
     }
+
     public FixtureResponse getMatchStatistic(long fixtureId) {
         try {
-            String json = restTemplate.getForObject(MATCH_STATISTICS_URL + fixtureId +  STATISTIC + "&"+ API_TOKEN, String.class);
+            String json = restTemplate.getForObject(MATCH_STATISTICS_URL + fixtureId + STATISTIC + "&" + API_TOKEN, String.class);
             return objectMapper.readValue(json, FixtureResponse.class);
+        } catch (Exception e) {
+            throw new RuntimeException("SportMonks API çağrısı başarısız oldu", e);
+        }
+    }
+    public String getMatchScore(long fixtureId) {
+        try {
+            String json = restTemplate.getForObject(MATCH_STATISTICS_URL + fixtureId + SCORE + "&" + API_TOKEN, String.class);
+            JsonNode rootNode = objectMapper.readTree(json);
+
+            // Access the "data" object
+            JsonNode dataNode = rootNode.path("data");
+
+            // Access the "scores" array
+            JsonNode scoresNode = dataNode.path("scores");
+
+            int homeScore = 0;
+            int awayScore = 0;
+
+            if (scoresNode.isArray()) {
+                for (JsonNode score : scoresNode) {
+                    String description = score.path("description").asText();
+                    String participant = score.path("score").path("participant").asText();
+                    int goals = score.path("score").path("goals").asInt();
+
+                    if ("CURRENT".equalsIgnoreCase(description)) {
+                        if ("home".equalsIgnoreCase(participant)) {
+                            homeScore = goals;
+                        } else if ("away".equalsIgnoreCase(participant)) {
+                            awayScore = goals;
+                        }
+                    }
+                }
+            }
+
+            return homeScore + " - " + awayScore;
+        } catch (Exception e) {
+            throw new RuntimeException("SportMonks API çağrısı başarısız oldu", e);
+        }
+    }
+
+    public int getMatchMinute(long fixtureId) {
+        try {
+            String json = restTemplate.getForObject(MATCH_STATISTICS_URL + fixtureId + MINUTE + "&" + API_TOKEN, String.class);
+            JsonNode rootNode = objectMapper.readTree(json);
+
+            // Access the "data" object
+            JsonNode dataNode = rootNode.path("data");
+
+            // Access the "periods" array
+            JsonNode periodsNode = dataNode.path("periods");
+            JsonNode stateNode = dataNode.path("state");
+            if ("HT".equalsIgnoreCase(stateNode.path("state").asText())) {
+                return 45;
+            }
+
+            if (periodsNode.isArray()) {
+                for (JsonNode period : periodsNode) {
+                    String description = period.path("description").asText();
+                    if ("2nd-half".equalsIgnoreCase(description)) {
+                        return period.path("minutes").asInt();
+                    }
+                }
+            }
+
+            // Return 0 if "2nd-half" is not found
+            return 0;
         } catch (Exception e) {
             throw new RuntimeException("SportMonks API çağrısı başarısız oldu", e);
         }
